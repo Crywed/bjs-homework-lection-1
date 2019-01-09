31f5903e706a755dfafe4df3dfe40791a1b0b885
Crywed <rgstarov@ya.ru>
2019-01-09 20:54:47 +1000

    lection-1

---
+++ variables/main.js
@@ -0,0 +1,26 @@
+// Первое задание
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+// Второе задание
+
+let algebra = 5;
+let geography = 4;
+let physics = 3;
+
+let averageScore = (algebra + geography + physics) / 3;
+
+console.log(averageScore);
+
+// Третье задание
+
+let name = 'Mary';
+let message = 'Hello, World! My name is ';
+
+console.log(message + `${name}` + ".");
